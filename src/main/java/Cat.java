6391f1f086e91ee682cd954355454e8d8ad06a0d
Admin <flenivetc@gmail.com>
2021-03-23 22:02:49 +0300

    Small fixes*

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,5 +1,10 @@
 public class Cat {
     {
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         EnumSingleton.INSTANCE.doWork();
     }
 }
