2bf9f87720e842732fdfa5c3c6aaf0fbea0dc447
Admin <flenivetc@gmail.com>
2021-03-24 13:06:48 +0300

    Stable v1.0 release))))

--- src/main/java/EnumSingleton.java
+++ src/main/java/EnumSingleton.java
@@ -2,6 +2,6 @@ public enum EnumSingleton {
     INSTANCE;
     private int NUMBER = 1;
     public synchronized void doWork(String s){
-        System.out.println("Enum " + (NUMBER++) + " from " + s + " cat");
+        System.out.println("Meow № " + (NUMBER++) + " from " + s + " cat");
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,39 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args){
-        for (int i = 0; i < 3; i++){
-            new Thread(new CatFactory(10,i+1)).start();
+        int threadsNumber = 0;
+        int catQuantity = 0;
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Введите количество потоков:");
+        try {
+            threadsNumber = Integer.parseInt(reader.readLine());
+        }catch (Exception e){}
+
+        System.out.println("Введите количество котов в каждом потоке:");
+        try {
+            catQuantity = Integer.parseInt(reader.readLine());
+        }catch (Exception e){}
+
+        if(threadsNumber == 0 || catQuantity == 0){
+            System.out.println("Одна из введенных величин некорректна. \nФабрика котиков будет запущена с аргументами по умолчанию (5 и 5). \nНаслаждайтесь.");
+            threadsNumber = 5;
+            catQuantity = 5;
+        } else {
+            System.out.println("Фабрика котиков будет запущена с аргументами (" + threadsNumber + " и " + catQuantity + "). \nНаслаждайтесь.");
+        }
+
+        try {
+            Thread.sleep(2500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < threadsNumber; i++){
+            new Thread(new CatFactory(catQuantity,i+1)).start();
         }
     }
 }
